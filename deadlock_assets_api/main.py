f89f87d05d0dc7b8d5f185988fe3debb6ee9760b
Manuel Raimann <raimannma@outlook.de>
2024-10-12 12:42:09 +0200

    Add GZip Compression

--- deadlock_assets_api/main.py
+++ deadlock_assets_api/main.py
@@ -3,6 +3,7 @@ import logging
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from prometheus_fastapi_instrumentator import Instrumentator
+from starlette.middleware.gzip import GZipMiddleware
 from starlette.requests import Request
 from starlette.responses import RedirectResponse, Response
 from starlette.staticfiles import StaticFiles
@@ -22,6 +23,7 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+app.add_middleware(GZipMiddleware, minimum_size=1000, compresslevel=5)
 
 Instrumentator().instrument(app).expose(app, include_in_schema=False)
 
