e886116f7fff5db0ea1a46f9be551fa2ab011ec4
Manuel Raimann <raimannma@outlook.de>
2024-10-15 00:52:42 +0200

    Fix

--- deadlock_assets_api/glob.py
+++ deadlock_assets_api/glob.py
@@ -1,3 +1,3 @@
 IMAGE_BASE_URL = "https://assets.deadlock-api.com/images"
-SVGS_BASE_URL = "https://assets.deadlock-api.com/svgs"
+SVGS_BASE_URL = "https://assets.deadlock-api.com/icons"
 VIDEO_BASE_URL = "https://assets.deadlock-api.com/videos"
